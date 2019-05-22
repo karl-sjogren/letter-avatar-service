0aafa379c7f790da6c74f17be7946c82bc9b80b5
Karl-Johan Sjogren <karl-johan.sjogren@openlibrarysolutions.com>
2019-05-22 13:44:37 +0200

    Wrote some text

--- src/routes/installing.svelte
+++ src/routes/installing.svelte
@@ -4,4 +4,41 @@
 
 <h1>Installing Letter Avatar Service</h1>
 
-<p>Installation instructions</p>
+<h2>Docker</h2>
+
+<p>The service can be installed in serveral ways. The easiest would be by using the <a href="https://hub.docker.com/r/karlsjogren/letter-avatars">docker image from Docker Hub</a>.</p>
+
+<code>
+docker pull karlsjogren/letter-avatars
+docker run -d -p 80:80 karlsjogren/letter-avatars
+</code>
+
+<h2>From GitHub release</h2>
+
+<p>Releases are available over at <a href="https://github.com/karl-sjogren/letter-avatar-service/releases">GitHub</a> and are built as Framework-dependent deployment packages.</p>
+
+<p>To run this .NET Core 3.0 Preview 5 Runtime (or above) has to be available on the system.</p>
+
+<p>To get it running simply extract the zip file and execute the correct binary depending on your system</p>
+
+<code>
+./LetterAvatarService # For OSX/Linux
+dotnet LetterAvatarService.dll # For Windows
+</code>
+
+<h2>From source</h2>
+
+<p>Clone the repository using GIT.</p>
+
+<code>
+git clone https://github.com/karl-sjogren/letter-avatar-service.git
+</code>
+
+<p>Or download the latest version as a <a href="https://github.com/karl-sjogren/letter-avatar-service/archive/master.zip">zip-file</a> and unzip it.</p>
+
+<p>Change into the LetterAvatarService folder and execute it using .Net CLI.</p>
+
+<code>
+cd src/LetterAvatarService
+dotnet run
+</code>
--- src/stores/name-store.js
+++ src/stores/name-store.js
@@ -2,8 +2,9 @@ import { writable } from 'svelte/store';
 import previewNames from './preview-names';
 
 function createName() {
-	const { subscribe, set, update } = writable('Letter Avatars', () => {
-    const interval = setInterval(() => {
+  let interval;
+  const { subscribe, set } = writable('Letter Avatars', () => {
+    interval = setInterval(() => {
       let randomName = previewNames[Math.floor(Math.random() * previewNames.length)];
       set(randomName);
     }, 500);
@@ -11,12 +12,12 @@ function createName() {
     return () => clearInterval(interval);
   });
 
-	return {
-		subscribe,
-		stopRandomGeneration: () => clearInterval(interval),
-		setName: name => set(name),
-		reset: () => set('Letter Avatars')
-	};
+  return {
+    subscribe,
+    stopRandomGeneration: () => clearInterval(interval),
+    setName: name => set(name),
+    reset: () => set('Letter Avatars')
+  };
 }
 
 export const name = createName();
